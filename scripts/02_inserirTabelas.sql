747beb95f02117ceca57371366e7523076faef97
Italo Bruno Avelino <52352651+ItaloBrunoM@users.noreply.github.com>
2025-06-21 22:37:04 -0300

    refactor(database): alteração no hash de senha

--- scripts/02_inserirTabelas.sql
+++ scripts/02_inserirTabelas.sql
@@ -6,7 +6,7 @@ INSERT INTO usuarios (id_pessoa, login, senha, role)
 VALUES (
     (SELECT id FROM pessoa WHERE cpf = '000.000.000-00'),
     'admin',
-    'hash_da_senha_admin', -- TROCAR PELA SENHA REAL EM HASH
+    '$argon2id$v=19$m=65536,t=3,p=4$kDuaASUDwi1X53S/6AN4WA$87EwqBFv3Ual+B8gjBH0xIlGkXflsT+hnBziryQ82r8', -- TROCAR PELA SENHA REAL EM HASH
     'admin'
 )
 ON CONFLICT (login) DO NOTHING;
@@ -18,7 +18,7 @@ INSERT INTO usuarios (id_pessoa, login, senha, role)
 VALUES (
     (SELECT id FROM pessoa WHERE cpf = '111.111.111-11'),
     'funcionario',
-    'hash_da_senha_funcionario', -- TROCAR PELA SENHA REAL EM HASH
+    '$argon2id$v=19$m=65536,t=3,p=4$Af2OI0iyCxIVIZDuXAHCbw$1CQjNyxWRCIT+YDURNn4m4TN+5RnmvNf+SWNzYnF83Y', -- TROCAR PELA SENHA REAL EM HASH
     'funcionario'
 )
 ON CONFLICT (login) DO NOTHING;
