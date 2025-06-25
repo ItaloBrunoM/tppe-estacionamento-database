eff1e0c87681e93ecc72ba4c5cef444cf556545f
Italo Bruno Avelino <52352651+ItaloBrunoM@users.noreply.github.com>
2025-06-25 13:24:35 -0300

    fix(database): correção scripts de criação

--- scripts/01_criarTabelas.sql
+++ scripts/01_criarTabelas.sql
@@ -1,20 +1,10 @@
-CREATE TYPE tipo_acesso AS ENUM ('por_tempo', 'diaria', 'evento', 'mensalista');
+CREATE TYPE tipo_acesso AS ENUM ('evento', 'hora', 'diaria'); 
 CREATE TYPE user_role AS ENUM ('admin', 'funcionario');
-
-CREATE TABLE estacionamento (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(255) UNIQUE NOT NULL, 
-    endereco TEXT,
-    total_vagas INT NOT NULL,
-    valor_primeira_hora NUMERIC(10, 2),
-    valor_demais_horas NUMERIC(10, 2),
-    valor_diaria NUMERIC(10, 2)
-);
 
 CREATE TABLE pessoa (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL, 
+    cpf VARCHAR(14) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE
 );
 
@@ -24,7 +14,21 @@ CREATE TABLE usuarios (
     login VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(255) NOT NULL,
     role user_role NOT NULL DEFAULT 'funcionario',
-    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id) ON DELETE CASCADE
+    admin_id INT,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES usuarios(id) ON DELETE SET NULL
+);
+
+CREATE TABLE estacionamento (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) UNIQUE NOT NULL,
+    endereco TEXT,
+    total_vagas INT NOT NULL,
+    valor_primeira_hora NUMERIC(10, 2),
+    valor_demais_horas NUMERIC(10, 2),
+    valor_diaria NUMERIC(10, 2),
+    admin_id INT, 
+    FOREIGN KEY (admin_id) REFERENCES usuarios(id) ON DELETE SET NULL
 );
 
 CREATE TABLE veiculo (
@@ -54,24 +58,24 @@ CREATE TABLE evento (
     hora_inicio TIME NOT NULL,
     hora_fim TIME NOT NULL,
     valor_acesso_unico NUMERIC(10, 2),
-    FOREIGN KEY (id_estacionamento) REFERENCES estacionamento(id) ON DELETE CASCADE
+    admin_id INT, 
+    FOREIGN KEY (id_estacionamento) REFERENCES estacionamento(id) ON DELETE CASCADE,
+    FOREIGN KEY (admin_id) REFERENCES usuarios(id) ON DELETE SET NULL
 );
 
 CREATE TABLE acesso (
     id SERIAL PRIMARY KEY,
-    id_estacionamento INT NOT NULL,
-    id_veiculo INT NOT NULL,
-    tipo tipo_acesso NOT NULL,
-    hora_entrada TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    placa VARCHAR(10) NOT NULL,
+    hora_entrada TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
     hora_saida TIMESTAMP WITH TIME ZONE,
-    valor_pago NUMERIC(10, 2),
-    id_acesso_evento INT,
-    id_acesso_mensalista INT,
-    -- ATUALIZAÇÃO AQUI:
+    valor_total NUMERIC(10, 2), 
+    tipo_acesso tipo_acesso NOT NULL DEFAULT 'hora',
+    id_estacionamento INT NOT NULL,
+    id_evento INT,
+    admin_id INT,
     FOREIGN KEY (id_estacionamento) REFERENCES estacionamento(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_veiculo) REFERENCES veiculo(id) ON DELETE CASCADE, 
-    FOREIGN KEY (id_acesso_evento) REFERENCES evento(id) ON DELETE CASCADE,
-    FOREIGN KEY (id_acesso_mensalista) REFERENCES cliente_mensalista(id) ON DELETE CASCADE
+    FOREIGN KEY (id_evento) REFERENCES evento(id) ON DELETE SET NULL, 
+    FOREIGN KEY (admin_id) REFERENCES usuarios(id) ON DELETE SET NULL 
 );
 
 CREATE TABLE faturamento (
@@ -79,5 +83,10 @@ CREATE TABLE faturamento (
     id_acesso INT NOT NULL,
     data_faturamento DATE NOT NULL DEFAULT CURRENT_DATE,
     valor NUMERIC(10, 2) NOT NULL,
-    FOREIGN KEY (id_acesso) REFERENCES acesso(id)
-);
+    FOREIGN KEY (id_acesso) REFERENCES acesso(id) ON DELETE CASCADE
+); 
+
+
+ALTER TABLE usuarios
+ADD CONSTRAINT fk_usuario_id_pessoa
+FOREIGN KEY (id_pessoa) REFERENCES pessoa(id) ON DELETE CASCADE; 
--- scripts/02_inserirTabelas.sql
+++ scripts/02_inserirTabelas.sql
@@ -2,23 +2,39 @@ INSERT INTO pessoa (nome, cpf, email)
 VALUES ('Administrador do Sistema', '000.000.000-00', 'admin@estacionamento.com')
 ON CONFLICT (cpf) DO NOTHING;
 
-INSERT INTO usuarios (id_pessoa, login, senha, role)
+INSERT INTO usuarios (id_pessoa, login, senha, role, admin_id)
 VALUES (
     (SELECT id FROM pessoa WHERE cpf = '000.000.000-00'),
     'admin',
-    '$argon2id$v=19$m=65536,t=3,p=4$kDuaASUDwi1X53S/6AN4WA$87EwqBFv3Ual+B8gjBH0xIlGkXflsT+hnBziryQ82r8', -- TROCAR PELA SENHA REAL EM HASH
-    'admin'
+    '$argon2id$v=19$m=65536,t=3,p=4$9n4vJQQgxFjLmVMK4VwrJQ$TW7+9i9ilThFKUi9ywPZMqVFefdXUquXVm+yfB06M68', -- Hash para 'admin'
+    'admin',
+    NULL
 )
 ON CONFLICT (login) DO NOTHING;
 
 INSERT INTO pessoa (nome, cpf, email)
 VALUES ('Funcionário Padrão', '111.111.111-11', 'funcionario@estacionamento.com')
 ON CONFLICT (cpf) DO NOTHING;
-INSERT INTO usuarios (id_pessoa, login, senha, role)
+INSERT INTO usuarios (id_pessoa, login, senha, role, admin_id)
 VALUES (
     (SELECT id FROM pessoa WHERE cpf = '111.111.111-11'),
     'funcionario',
-    '$argon2id$v=19$m=65536,t=3,p=4$Af2OI0iyCxIVIZDuXAHCbw$1CQjNyxWRCIT+YDURNn4m4TN+5RnmvNf+SWNzYnF83Y', -- TROCAR PELA SENHA REAL EM HASH
-    'funcionario'
+    '$argon2id$v=19$m=65536,t=3,p=4$E0LIeW+NEUIo5ZyTEqL0vg$ndDEkfHXqqUHgnsQUgh6IoJyXFe7fUS5h33vnt+udzk', -- Hash para 'funcionario'
+    'funcionario',
+    (SELECT id FROM usuarios WHERE login = 'admin')
+)
+ON CONFLICT (login) DO NOTHING;
+
+INSERT INTO pessoa (nome, cpf, email)
+VALUES ('Administrador Teste 2', '000.000.000-01', 'admin2@estacionamento.com')
+ON CONFLICT (cpf) DO NOTHING;
+
+INSERT INTO usuarios (id_pessoa, login, senha, role, admin_id)
+VALUES (
+    (SELECT id FROM pessoa WHERE cpf = '000.000.000-01'),
+    'admin2',
+    '$argon2id$v=19$m=65536,t=3,p=4$TgnBWKs1xhiD8J4TQsgZIw$hQkkJWI0BdGzfsry8XzUEx1edol8Wsj7HgOLxEgmG7E', -- Hash para 'admin2'
+    'admin',
+    NULL
 )
 ON CONFLICT (login) DO NOTHING;
